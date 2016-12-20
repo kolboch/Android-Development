eb06830586a1335c26877356ace4b16e43fcab5b
Karol Bochynski <kolboch@gmail.com>
2016-12-20 01:00:15 +0100

    simple button changing textView state

---
+++ JustJava/app/src/main/java/dev/kb/justjava/MainActivity.java
@@ -0,0 +1,31 @@
+package dev.kb.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * method called when order button is clicked
+     * @param view
+     */
+    public void submitOrder(View view){
+        display(89 * 5 + 167);
+    }
+    /**
+     * this method displays the given quantity value on the screen
+     */
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+}
