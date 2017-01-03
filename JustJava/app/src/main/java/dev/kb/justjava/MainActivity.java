ae703899b888ed4dd74458e9c313c7eea10b5deb
Karol Bochynski <kolboch@gmail.com>
2017-01-03 17:51:22 +0100

    + and - button with UI refreshing

--- JustJava/app/src/main/java/dev/kb/justjava/MainActivity.java
+++ JustJava/app/src/main/java/dev/kb/justjava/MainActivity.java
@@ -1,16 +1,24 @@
 package dev.kb.justjava;
 
+import android.icu.math.BigDecimal;
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int COFFEE_PRICE = 5;
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        refreshQuantityAndPrice();
     }
 
     /**
@@ -18,14 +26,43 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void submitOrder(View view){
-        display(89 * 5 + 167);
+        displayPrice(new BigDecimal(quantity * COFFEE_PRICE));
+    }
+
+    /**
+     * refreshes textViews of quantity and Price
+     */
+    private void refreshQuantityAndPrice(){
+        displayQuantity();
+        displayPrice(new BigDecimal(quantity * COFFEE_PRICE));
     }
     /**
-     * this method displays the given quantity value on the screen
+     * increments quantity ( current quantity + 1 ), called when + button is clicked
      */
-    private void display(int number){
+    public void incrementQuantity(View view){
+        quantity++;
+        refreshQuantityAndPrice();
+    }
+
+    /**
+     * updates quantity textView
+     */
+    private void displayQuantity(){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + quantity);
+    }
+
+    /**
+     * decrements quantity ( current quantity - 1), called when - button is clicked
+     */
+    public void decrementQuantity(View view){
+        quantity--;
+        refreshQuantityAndPrice();
+    }
+
+    private void displayPrice(BigDecimal price){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
     }
 
 }
