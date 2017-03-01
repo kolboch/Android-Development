180e592ca4df52d2fa093f58623583e6e386b87d
Karol Bochynski <kolboch@gmail.com>
2017-03-01 11:39:52 +0100

    Contract class for db

--- Shelter/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ Shelter/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,12 +1,29 @@
 package com.example.android.pets.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Karol on 2017-03-01.
  */
 
 public final class PetContract {
 
+    //private constructor to disable option of instantiating class by mistake
     private PetContract(){
 
+    }
+
+    public static class PetEntry implements BaseColumns{
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID; // in db must be the same as BaseColumns._ID const.
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+        public static final int GENDER_UNKNOWN = 0;
     }
 }
