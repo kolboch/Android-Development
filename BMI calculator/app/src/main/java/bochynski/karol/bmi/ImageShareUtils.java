7d466e9294a96225600e07bc6eb15bc1069fdf77
Karol Bochynski <kolboch@gmail.com>
2017-04-07 23:09:00 +0200

    Saving last valid result, Dialogs for share and save ( screenshot or text)

--- BMI calculator/app/src/main/java/bochynski/karol/bmi/ImageShareUtils.java
+++ BMI calculator/app/src/main/java/bochynski/karol/bmi/ImageShareUtils.java
@@ -6,7 +6,6 @@ import android.os.Environment;
 import android.view.View;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -29,34 +28,27 @@ public class ImageShareUtils {
         return screenBitmap;
     }
 
-    public static File storeBitmapToFile(Bitmap toStore, String dirFolderName, String fileName) {
+    public static File storeBitmapToFile(Bitmap toStore, String dirFolderName, String fileName) throws IOException {
         String folderDir = EXTERNAL_ABS_DIR_PATH + "/" + dirFolderName;
         File folder = new File(folderDir);
-        if(!folder.exists()){
+        if (!folder.exists()) {
             folder.mkdir();
         }
         File file = new File(folder, fileName + PNG_EXTENSION);
         int i = 1;
-        while(file.exists()){
+        while (file.exists()) {
             file = null;
             file = new File(folder, fileName + "(" + i + ")" + PNG_EXTENSION);
             i++;
         }
-        try{
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            boolean compressResult = toStore.compress(CompressFormat.PNG, 100, fileOutputStream); // when png quality is ignored
-            fileOutputStream.flush();
-            fileOutputStream.close();
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        boolean compressResult = toStore.compress(CompressFormat.PNG, 100, fileOutputStream); // when png quality is ignored
+        fileOutputStream.flush();
+        fileOutputStream.close();
         return file;
     }
 
-    public static String generateFileName(){
+    public static String generateFileName() {
         Date d = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         return sdf.format(d);
--- BMI calculator/app/src/main/java/bochynski/karol/bmi/MainActivity.java
+++ BMI calculator/app/src/main/java/bochynski/karol/bmi/MainActivity.java
@@ -1,15 +1,21 @@
 package bochynski.karol.bmi;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,6 +27,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 import bochynski.karol.bmi.exceptions.InvalidHeightException;
@@ -33,15 +40,27 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     //TODO hide show save/share when no result yet
-    //TODO remove useless intents
-    @BindView(R.id.BMI_result) TextView resultBMI;
-    @BindView(R.id.BMI_result_description) TextView resultBMIdescription;
-    @BindView(R.id.count_bmi_button) Button countBmiBttn;
-    @BindString(R.string.dir_screenshots_folder) String screenshotsDirFolder;
-    @BindString(R.string.screenshot_file_default_name) String fileDefaultName;
+    @BindView(R.id.BMI_result)
+    TextView resultBMI;
+    @BindView(R.id.BMI_result_description)
+    TextView resultBMIdescription;
+    @BindView(R.id.count_bmi_button)
+    Button countBmiBttn;
+    @BindString(R.string.dir_screenshots_folder)
+    String screenshotsDirFolder;
+    @BindString(R.string.screenshot_file_default_name)
+    String fileDefaultName;
+    @BindString(R.string.saved_toast)
+    String savedToast;
+    @BindString(R.string.share_BMI_result_text_title)
+    String shareBMIResultTextTitle;
+    @BindString(R.string.share_using)
+    String shareWith;
     private ICountBMI counter;
     private String BMI_RESULT_SAVED = "BMI_RESULT";
     private String BMI_RESULT_DESCRIPTION_SAVED = "BMI_RESULT_DESCRIPTION";
+    private static final String PREFERENCES_NAME = "BMI_preferences";
+    private static final String MAIN_LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         addFragmentIfNotPresent();
         setListeners();
+        setResultIfSaved();
     }
 
     private void addFragmentIfNotPresent() {
@@ -118,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_item_kg_meters:
                 if (!isFragmentMetric()) {
                     replaceInputDataFrame(InputMetricFragment.newInstance());
+                    resetResultsViews();
                     hideResultsViews();
                     makeShortToast(R.string.menu_units_kg_m);
                 }
@@ -125,19 +146,16 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_item_lb_inches:
                 if (!isFragmentImperial()) {
                     replaceInputDataFrame(InputImperialFragment.newInstance());
+                    resetResultsViews();
                     hideResultsViews();
                     makeShortToast(R.string.menu_units_lb_in);
                 }
                 return true;
             case R.id.menu_item_share:
-                //TODO handle sharing BMI result
-                makeShortToast(R.string.menu_share);
+                showShareDialog();
                 return true;
             case R.id.menu_item_save:
-                //TODO handle saving screenshot or data to memory
-                //TODO just result or image question
-                takeScreenShotAndSave();
-                makeShortToast(R.string.menu_save);
+                showSaveDialog();
                 return true;
             case R.id.menu_item_about_author:
                 Intent i = new Intent(this, AuthorInfoActivity.class);
@@ -145,6 +163,43 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveToSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        Editor editor = preferences.edit();
+        boolean saved = false;
+        if (resultBMI != null && !TextUtils.isEmpty(resultBMI.getText())) {
+            editor.putString(BMI_RESULT_SAVED, (String) resultBMI.getText());
+            editor.commit();
+            if (resultBMIdescription != null && !TextUtils.isEmpty(resultBMIdescription.getText())) {
+                editor.putString(BMI_RESULT_DESCRIPTION_SAVED, (String) resultBMIdescription.getText());
+                editor.commit();
+            }
+            saved = true;
+        }
+        if (saved) {
+            makeShortToast(R.string.saved_BMI_results);
+        }
+    }
+
+    private void setResultIfSaved() {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        String result, description;
+        result = preferences.getString(BMI_RESULT_SAVED, "");
+        description = preferences.getString(BMI_RESULT_DESCRIPTION_SAVED, "");
+        boolean restored = false;
+        if (!TextUtils.isEmpty(result)) {
+            resultBMI.setText(result);
+            setResultBMIColor(result);
+            restored = true;
+        }
+        if (!TextUtils.isEmpty(description)) {
+            resultBMIdescription.setText(description);
+        }
+        if(restored){
+            showResultViews();
         }
     }
 
@@ -171,6 +226,15 @@ public class MainActivity extends AppCompatActivity {
     private void showResultViews() {
         resultBMI.setVisibility(View.VISIBLE);
         resultBMIdescription.setVisibility(View.VISIBLE);
+    }
+
+    private void resetResultsViews(){
+        if(resultBMI != null){
+            resultBMI.setText("");
+        }
+        if(resultBMIdescription != null){
+            resultBMIdescription.setText("");
+        }
     }
 
     private void hideResultsViews() {
@@ -228,17 +292,50 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void makeShortToast(int messageResource){
+    private void makeShortToast(int messageResource) {
         Toast.makeText(this, messageResource, Toast.LENGTH_SHORT).show();
     }
 
-    private void takeScreenShotAndSave(){
+    private File saveScreenshot() {
+        if(resultBMI == null || TextUtils.isEmpty(resultBMI.getText())){
+            makeShortToast(R.string.no_result_available);
+            return null;
+        }
         Bitmap bm = ImageShareUtils.getScreenshot(findViewById(R.id.activity_main));
-        File saved = ImageShareUtils.storeBitmapToFile(bm, screenshotsDirFolder, fileDefaultName + ImageShareUtils.generateFileName());
-        Toast.makeText(this, "Saved under: " + saved.getPath(), Toast.LENGTH_SHORT).show();
+        File saved;
+        try {
+            saved = ImageShareUtils.storeBitmapToFile(bm, screenshotsDirFolder, fileDefaultName + ImageShareUtils.generateFileName());
+        } catch (IOException e) {
+            makeShortToast(R.string.exception_failed_to_save_screenshot);
+            saved = null; // to be sure that successful saving message is not displayed
+            Log.e(MAIN_LOG_TAG, e.getMessage());
+        }
+        if (saved != null) {
+            Toast.makeText(this, savedToast + " " + saved.getPath(), Toast.LENGTH_SHORT).show();
+        }
+        return saved;
     }
 
-    private void shareFile(File f){
+    private void shareScreenshot() {
+        File saved = saveScreenshot();
+        if (saved != null) {
+            shareImage(saved);
+        }
+    }
+
+    private void shareText(String text) {
+        if(TextUtils.isEmpty(text)){
+            makeShortToast(R.string.no_result_available);
+        }
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_SUBJECT, shareBMIResultTextTitle);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, shareWith));
+    }
+
+    private void shareImage(File f) {
         Uri uri = Uri.fromFile(f);
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
@@ -246,9 +343,71 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_STREAM, uri);
         try {
             startActivity(Intent.createChooser(intent, getResources().getString(R.string.share_apps_chooser_title)));
-        }
-        catch (ActivityNotFoundException e){
+        } catch (ActivityNotFoundException e) {
             makeShortToast(R.string.exception_no_app_to_handle_share);
         }
+    }
+
+    private String getResultBMI(){
+        String result = "";
+        if(resultBMI != null){
+            result = (String)resultBMI.getText();
+        }
+        return result;
+    }
+
+    private void showSaveDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage(R.string.save_alert_dialog_title);
+        alertBuilder.setPositiveButton(R.string.save_alert_dialog_image, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                saveScreenshot();
+            }
+        });
+        alertBuilder.setNeutralButton(R.string.save_alert_dialog_last_result, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                saveToSharedPreferences();
+            }
+        });
+        alertBuilder.setNegativeButton(R.string.alert_dialog_cancel, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(dialog != null){
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog dialog = alertBuilder.create();
+        dialog.show();
+    }
+
+    private void showShareDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage(R.string.save_alert_dialog_title);
+        alertBuilder.setPositiveButton(R.string.save_alert_dialog_image, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                shareScreenshot();
+            }
+        });
+        alertBuilder.setNeutralButton(R.string.save_alert_dialog_last_result, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String result = getResultBMI();
+                shareText(result);
+            }
+        });
+        alertBuilder.setNegativeButton(R.string.alert_dialog_cancel, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(dialog != null){
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog dialog = alertBuilder.create();
+        dialog.show();
     }
 }
